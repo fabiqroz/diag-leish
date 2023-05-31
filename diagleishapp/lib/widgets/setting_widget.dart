dea4a7a10a868c88692f24a6d2a2f723ce860489
Alan Pereira <alan.pereira.dev@gmail.com>
2023-05-31 17:02:14 -0300

    add informative movies

--- diagleishapp/lib/widgets/setting_widget.dart
+++ diagleishapp/lib/widgets/setting_widget.dart
@@ -9,17 +9,27 @@ class SettingWidget extends StatefulWidget {
 }
 
 class _SettingWidgetState extends State<SettingWidget> {
-  late YoutubePlayerController _controller;
+  late YoutubePlayerController controllerV1;
+  late YoutubePlayerController controllerV2;
 
   @override
   void initState() {
-    _controller = YoutubePlayerController(
-      initialVideoId: '-BYWbosiYlw',
-      flags: YoutubePlayerFlags(
-        autoPlay: true,
-        mute: true,
+    controllerV1 = YoutubePlayerController(
+      initialVideoId: 'L3zioEy_QWg',
+      flags: const YoutubePlayerFlags(
+        autoPlay: false,
+        mute: false,
       ),
     );
+
+    controllerV2 = YoutubePlayerController(
+      initialVideoId: 's0yiOq4bnNQ',
+      flags: const YoutubePlayerFlags(
+        autoPlay: false,
+        mute: false,
+      ),
+    );
+
     super.initState();
   }
 
@@ -27,27 +37,54 @@ class _SettingWidgetState extends State<SettingWidget> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Container(
-        child: Column(
-          children: [
-            Text("Links Informativos"),
-            SizedBox(
-              height: 10,
+      child: Column(
+        children: [
+          const Text(
+            "O que é a Leishmaniose ?",
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          YoutubePlayer(
+            controller: controllerV1,
+            showVideoProgressIndicator: true,
+            progressIndicatorColor: Colors.purple,
+            progressColors: const ProgressBarColors(
+              playedColor: Colors.purple,
+              handleColor: Colors.purpleAccent,
             ),
-            YoutubePlayer(
-              controller: _controller,
-              showVideoProgressIndicator: true,
-              progressIndicatorColor: Colors.amber,
-              progressColors: ProgressBarColors(
-                playedColor: Colors.amber,
-                handleColor: Colors.amberAccent,
-              ),
-              onReady: () {
-                _controller.addListener(() {});
-              },
+            onReady: () {
+              controllerV1.addListener(() {});
+            },
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Divider(
+            color: Theme.of(context).colorScheme.secondary,
+          ),
+          const Text(
+            "Diagnóstico, prevenção e tratamento da Leishmaniose Visceral",
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          YoutubePlayer(
+            controller: controllerV2,
+            showVideoProgressIndicator: true,
+            progressIndicatorColor: Colors.purple,
+            progressColors: const ProgressBarColors(
+              playedColor: Colors.purple,
+              handleColor: Colors.purpleAccent,
             ),
-          ],
-        ),
+            onReady: () {
+              controllerV1.addListener(() {});
+            },
+          ),
+        ],
       ),
     );
   }
